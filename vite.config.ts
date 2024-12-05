387ed3f9e7a86668ebc0bf695c152a5a06a41e5f
jonas <jonasbrauer@seznam.cz>
2024-12-05 09:33:29 +0100

    feat: PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
@@ -14,7 +15,43 @@ export default defineConfig({
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar(),
-    UnoCSS()
+    UnoCSS(),
+    VitePWA({
+      manifest: {
+        name: 'THE Template',
+        short_name: 'Template',
+        description: 'THE best template of all time',
+        theme_color: '#155cdb',
+        background_color: '#155cdb',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
